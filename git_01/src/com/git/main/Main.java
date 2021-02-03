34c5a5bad16086a85331646ea8bdcbd6f3df15c7
son <dmswl4388@naver.com>
2021-02-03 15:43:40 +0900

    commit

--- git_01/src/com/git/main/Main.java
+++ git_01/src/com/git/main/Main.java
@@ -2,6 +2,9 @@ package com.git.main;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello Git !!");
+		System.out.println("11111111");
+		System.out.println("22222222222222222");
+		System.out.println("Hello Spring");
 	}
+
 }
